9d04dc6f9f534d79c1e11668292d1e9b5b57d82a
TheSleepyTurtle <s.krithick22@gmail.com>
2021-11-11 08:53:22 +0000

    hi

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>It works</h1>')
-})
-
-app.get('/greet/:name', (req, res) => {
-    res.send(`Hello, ${req.params.name}`)
+    res.send('Hello, World')
 })
 
 app.listen(3000)
